3935a785307675b7ea402a438bd4e2347efc9cc7
ruansheng <1156341958@qq.com>
2023-10-26 10:30:55 +0800

    优化todolist细节

--- src/views/example/index.vue
+++ src/views/example/index.vue
@@ -1,23 +1,29 @@
 <template>
   <div class="todo">
-    <div>
-      <input placeholder="输入一些想法之类的吧" v-model="newTodo" />
-      <button @click="addItem">+</button>
-    </div>
-    <div v-if="filterData.length">
-      <ul>
-        <li v-for="todo in filterData" :key="todo.id">
-          <span>{{ todo.id }}</span>
-          <input type="checkbox" v-model="todo.done" />
-          <span :class="{ done: todo.done }">{{ todo.context }}</span>
-          <button @click="removeItem(todo)">X</button>
+    <form @submit.prevent="addItem" id="todoform">
+      <input placeholder="Enter your todo" v-model="newTodo" />
+      <ul v-if="filterData.length" class="todos">
+        <li
+          @click="todo.done = !todo.done"
+          @contextmenu.prevent="removeItem(todo)"
+          :class="{ completed: todo.done }"
+          v-for="todo in filterData"
+          :key="todo.id"
+        >
+          <span>{{ todo.context }}</span>
         </li>
       </ul>
-    </div>
-    <div v-else>还没有待办事项，感觉添加一条代办事项吧~</div>
-    <button @click="hideCompleted = !hideCompleted">
-      {{ hideCompleted ? '展示所有' : '隐藏完成' }}
+      <ul class="empty" v-else>
+        empty todolist
+      </ul>
+    </form>
+    <button v-if="filterData.length" @click="hideCompleted = !hideCompleted">
+      {{ hideCompleted ? 'show all' : 'hideCompleted' }}
     </button>
+    <small
+      >Left click to toggle completed. <br />
+      Right click to delete todo</small
+    >
   </div>
 </template>
 
@@ -25,7 +31,7 @@
 import { useTodoStore } from '@/stores/todo'
 const store = useTodoStore()
 const { todoData, id } = storeToRefs(store)
-const hideCompleted = ref(false)
+let hideCompleted = ref(false)
 const newTodo = ref('')
 
 let filterData = computed(() => {
@@ -64,5 +70,71 @@ function removeItem(todo: TodoItem) {
   color: var(--c-text);
   padding: calc(var(--w-space) * 3) 0;
   margin: 0 auto;
+  background-color: #f5f5f5;
+}
+
+#todoform {
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  max-width: 100%;
+  width: 400px;
+  > input {
+    text-align: center;
+    border: none;
+    color: #444;
+    font-size: 2rem;
+    padding: 1rem 2rem;
+    display: block;
+    width: 100%;
+    padding: 0;
+    ::placeholder {
+      color: #d5d5d5;
+    }
+    :focus {
+      outline-color: rgb(179, 131, 226);
+    }
+  }
+}
+
+.todos {
+  background-color: #fff;
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+  li {
+    border-top: 1px solid #e5e5e5;
+    cursor: pointer;
+    font-size: 1.5rem;
+    padding: 1rem 2rem;
+  }
+  li.completed {
+    color: #b6b6b6;
+    text-decoration: line-through;
+  }
+}
+
+.empty {
+  color: #444;
+  background-color: #fff;
+  padding: 0;
+  margin: 0;
+  font-size: 1.5rem;
+  padding: 1rem 2rem;
+  text-align: center;
+}
+small {
+  color: #b5b5b5;
+  margin-top: 3rem;
+  text-align: center;
+}
+
+button {
+  width: 400px;
+  height: 40px;
+  background-color: #fff;
+  border: 0;
+  border-radius: 5px;
+  margin-top: 20px;
+  cursor: pointer;
+  font-size: 1rem;
 }
 </style>
